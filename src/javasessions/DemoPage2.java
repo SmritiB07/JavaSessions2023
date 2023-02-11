9262dd491b1df582a0f7597bab1ceae92d0ed279
SmritiB07 <gpsim0703@gmail.com>
2023-02-11 14:58:58 +0530

    added demo page class for Cart Page

---
+++ src/javasessions/DemoPage2.java
@@ -0,0 +1,15 @@
+package javasessions;
+
+public class DemoPage2 {
+	
+	public void addToCart() {
+		System.out.println("add to cart");
+	}
+
+	public static void main(String[] args) {
+		String welcome="hello world";
+		System.out.println(welcome);
+
+	}
+
+}
