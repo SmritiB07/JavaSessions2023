426c1e0f5b27bf0d8605a15595c51a1f3476c43b
SmritiB07 <gpsim0703@gmail.com>
2023-02-11 14:25:33 +0530

    added new class file

---
+++ src/javasessions/DummyPage.java
@@ -0,0 +1,17 @@
+package javasessions;
+
+public class DummyPage {
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		int i=100;
+		System.out.println(i);
+	}
+
+}
