3e106f0e6682b02458f76191c9d868cec51cf18e
SmritiB07 <gpsim0703@gmail.com>
2023-02-11 14:16:21 +0530

    Updated Demo Page

--- src/javasessions/DemoPage.java
+++ src/javasessions/DemoPage.java
@@ -1,9 +1,14 @@
 package javasessions;
 
 public class DemoPage {
+	
+	public void addkey() {
+		System.out.println("java sessions keys==========");
+	}
 
 	public static void main(String[] args) {
 System.out.println("=====demo page======");
+
 
 	}
 
